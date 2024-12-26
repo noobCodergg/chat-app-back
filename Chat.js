87729c9f4d59d6894117edf27fb548212d8a2164
noobCodergg <113461375+noobCodergg@users.noreply.github.com>
2024-12-26 19:36:34 +0600

    Update Chat.js

--- Chat.js
+++ Chat.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const accountSchema = new mongoose.Schema({
-  sender: String,
-  receiver: String,
-  content: String,
-});
+const accountSchema = new mongoose.Schema(
+  {
+    sender: String,
+    receiver: String,
+    content: String,
+  },
+  { timestamps: true } 
+);
 
 module.exports = mongoose.model("chats", accountSchema);
